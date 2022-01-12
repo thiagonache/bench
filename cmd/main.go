ea32df976db929015836cb026981247e0e023149
Thiago Nache Carvalho <thiagonbcarvalho@gmail.com>
2022-01-12 16:46:18 -0300

    Does not require user to set a parameter for the url

--- cmd/main.go
+++ cmd/main.go
@@ -3,20 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/thiagonache/simplebench"
 )
 
 func main() {
-	var url string
 	var reqs int
-	flag.StringVar(&url, "u", "", "url to run benchmark")
 	flag.IntVar(&reqs, "r", 1, "number of requests to be performed in the benchmark")
 	flag.Parse()
-	if url == "" {
-		flag.Usage()
-		return
+	if len(os.Args) <= 1 {
+		fmt.Println("Please, inform an url to benchmark")
+		os.Exit(1)
 	}
+	url := os.Args[1]
 	loadgen, err := simplebench.NewLoadGen(url, simplebench.WithRequests(reqs))
 	if err != nil {
 		panic(fmt.Errorf("error creating NewLoadGen object: %v", err))
